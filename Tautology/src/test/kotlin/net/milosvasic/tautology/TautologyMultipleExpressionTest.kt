90197c74897beb5ecb81d5bf47537aca2a3327a5
Milos Vasic <milos85vasic@gmail.com>
2017-04-19 12:14:28 +0200

    Tautology: Working on multiple expression test.

--- Tautology/src/test/kotlin/net/milosvasic/tautology/TautologyMultipleExpressionTest.kt
+++ Tautology/src/test/kotlin/net/milosvasic/tautology/TautologyMultipleExpressionTest.kt
@@ -1,15 +1,35 @@
 package net.milosvasic.tautology
 
+import org.junit.Before
 import org.junit.Test
 
 class TautologyMultipleExpressionTest {
 
     val tautology = Tautology()
+    val dataSets = mutableListOf<DataSet>()
+
+    @Before
+    fun prepareDataSets() {
+        // ((1 == 1) || (1 == 1)) && (1 >= 1)
+        dataSets.add(DataSet(1, 1, 1, true))
+    }
 
     @Test
     fun testTautologyMultipleExpression() {
-        
+        // ((a == b) || (a == c)) && (c >= a)
+        dataSets.forEach {
+            (a, b, c, result) ->
+            println("[ $a ][ $b ][ $c ][ $result ]")
+            
+        }
     }
+
+    data class DataSet(
+            val a: Int,
+            val b: Int,
+            val c: Int,
+            val result: Boolean
+    )
 
 }
 
