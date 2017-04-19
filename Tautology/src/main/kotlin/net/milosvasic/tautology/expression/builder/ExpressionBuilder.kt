1d355dadc58fcfedd1e4fe31926700cfc57e51fa
Milos Vasic <milos85vasic@gmail.com>
2017-04-19 12:42:40 +0200

    Tautology: Working on expression builder.

--- Tautology/src/main/kotlin/net/milosvasic/tautology/expression/builder/ExpressionBuilder.kt
+++ Tautology/src/main/kotlin/net/milosvasic/tautology/expression/builder/ExpressionBuilder.kt
@@ -1,8 +1,14 @@
 package net.milosvasic.tautology.expression.builder
 
+import net.milosvasic.tautology.expression.Expression
+
 class ExpressionBuilder {
 
+    fun build(): List<Expression> {
+        val expressions = mutableListOf<Expression>()
 
+        return expressions
+    }
 
 }
 
