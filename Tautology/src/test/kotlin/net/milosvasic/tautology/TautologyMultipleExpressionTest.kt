7a05973dde7290600b434b8b96e35013a6dcb09d
Milos Vasic <milos85vasic@gmail.com>
2017-04-19 12:30:38 +0200

    Tautology: Working on multiple expression test.

--- Tautology/src/test/kotlin/net/milosvasic/tautology/TautologyMultipleExpressionTest.kt
+++ Tautology/src/test/kotlin/net/milosvasic/tautology/TautologyMultipleExpressionTest.kt
@@ -1,5 +1,11 @@
 package net.milosvasic.tautology
 
+import net.milosvasic.tautology.expression.BooleanExpression
+import net.milosvasic.tautology.expression.Expression
+import net.milosvasic.tautology.expression.ExpressionValue
+import net.milosvasic.tautology.expression.MultipleExpression
+import net.milosvasic.tautology.operator.Operator
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
@@ -19,8 +25,43 @@ class TautologyMultipleExpressionTest {
         // ((a == b) || (a == c)) && (c >= a)
         dataSets.forEach {
             (a, b, c, result) ->
-            println("[ $a ][ $b ][ $c ][ $result ]")
-            
+            println("(($a == $b) || ($a == $c)) && ($c >= $a) expects: $result")
+
+            val expressions = mutableListOf<Expression>()
+
+            val abValue = object : ExpressionValue<Boolean> {
+                override fun getValue(): Boolean {
+                    return a == b
+                }
+            }
+            val acValue = object : ExpressionValue<Boolean> {
+                override fun getValue(): Boolean {
+                    return a == c
+                }
+            }
+            val caValue = object : ExpressionValue<Boolean> {
+                override fun getValue(): Boolean {
+                    return c >= a
+                }
+            }
+
+            val abExpression = BooleanExpression(abValue, Operator.OR())
+            val acExpression = BooleanExpression(acValue)
+            val caExpression = BooleanExpression(caValue)
+
+            val multiple = MultipleExpression()
+            multiple.expressions.add(abExpression)
+            multiple.expressions.add(acExpression)
+
+            val leftExpression = BooleanExpression(multiple, Operator.AND())
+            expressions.add(leftExpression)
+            expressions.add(caExpression)
+
+            if (result) {
+                Assert.assertTrue(tautology.evaluate(expressions))
+            } else {
+                Assert.assertFalse(tautology.evaluate(expressions))
+            }
         }
     }
 
