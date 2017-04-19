3c745924dbc62a9f80319455fe2a226a80f26053
Milos Vasic <milos85vasic@gmail.com>
2017-04-19 10:54:36 +0200

    Tautology: Tautology test.

---
+++ Tautology/src/test/kotlin/net/milosvasic/tautology/TautologyTest.kt
@@ -0,0 +1,69 @@
+package net.milosvasic.tautology
+
+import net.milosvasic.logger.SimpleLogger
+import net.milosvasic.tautology.expression.BooleanExpression
+import net.milosvasic.tautology.expression.ExpressionValue
+import net.milosvasic.tautology.operator.Operator
+import org.junit.Assert
+import org.junit.Test
+
+class TautologyTest {
+
+    val tautology = Tautology()
+
+    @Test
+    fun testTautology(){
+        val falseBooleanExpressionValue = object : ExpressionValue<Boolean> {
+            override fun getValue(): Boolean {
+                return false
+            }
+        }
+
+        val trueBooleanExpressionValue = object : ExpressionValue<Boolean> {
+            override fun getValue(): Boolean {
+                return true
+            }
+        }
+
+        val simpleBoolean = BooleanExpression(falseBooleanExpressionValue)
+        val simpleBoolean2 = BooleanExpression(trueBooleanExpressionValue)
+        val simpleBooleanWithNegation = BooleanExpression(falseBooleanExpressionValue, null, Operator.NOT())
+        val simpleBooleanWithOrOperation = BooleanExpression(falseBooleanExpressionValue, Operator.OR())
+        val simpleBooleanWithAndOperation = BooleanExpression(falseBooleanExpressionValue, Operator.AND())
+
+        // false == false
+        tautology.expressions.add(simpleBoolean)
+        Assert.assertFalse(tautology.evaluate())
+
+        // !false == true
+        tautology.expressions.clear()
+        tautology.expressions.add(simpleBooleanWithNegation)
+        Assert.assertTrue(tautology.evaluate())
+
+        // false && true == false
+        tautology.expressions.clear()
+        tautology.expressions.add(simpleBooleanWithAndOperation)
+        tautology.expressions.add(simpleBoolean2)
+        Assert.assertFalse(tautology.evaluate())
+
+        // false || true == true
+        tautology.expressions.clear()
+        tautology.expressions.add(simpleBooleanWithOrOperation)
+        tautology.expressions.add(simpleBoolean2)
+        Assert.assertTrue(tautology.evaluate())
+
+        // false && false == false
+        tautology.expressions.clear()
+        tautology.expressions.add(simpleBooleanWithAndOperation)
+        tautology.expressions.add(simpleBoolean)
+        Assert.assertFalse(tautology.evaluate())
+
+        // false || false == false
+        tautology.expressions.clear()
+        tautology.expressions.add(simpleBooleanWithOrOperation)
+        tautology.expressions.add(simpleBoolean)
+        Assert.assertFalse(tautology.evaluate())
+
+    }
+
+}
--- Tautology_Tryout/src/main/kotlin/net/milosvasic/tryout/tautology/Main.kt
+++ Tautology_Tryout/src/main/kotlin/net/milosvasic/tryout/tautology/Main.kt
@@ -1,66 +1,8 @@
 package net.milosvasic.tryout.tautology
 
-import net.milosvasic.logger.SimpleLogger
-import net.milosvasic.tautology.Tautology
-import net.milosvasic.tautology.expression.BooleanExpression
-import net.milosvasic.tautology.expression.ExpressionValue
-import net.milosvasic.tautology.operator.Operator
-
 fun main(args: Array<String>) {
 
-    val logger = SimpleLogger()
-    val tautology = Tautology()
 
-    val falseBooleanExpressionValue = object : ExpressionValue<Boolean> {
-        override fun getValue(): Boolean {
-            return false
-        }
-    }
-
-    val trueBooleanExpressionValue = object : ExpressionValue<Boolean> {
-        override fun getValue(): Boolean {
-            return true
-        }
-    }
-
-    val simpleBoolean = BooleanExpression(falseBooleanExpressionValue)
-    val simpleBoolean2 = BooleanExpression(trueBooleanExpressionValue)
-    val simpleBooleanWithNegation = BooleanExpression(falseBooleanExpressionValue, null, Operator.NOT())
-    val simpleBooleanWithOrOperation = BooleanExpression(falseBooleanExpressionValue, Operator.OR())
-    val simpleBooleanWithAndOperation = BooleanExpression(falseBooleanExpressionValue, Operator.AND())
-
-    // false == false
-    tautology.expressions.add(simpleBoolean)
-    logger.v("", "Evaluating simple boolean [ ${tautology.evaluate()} ]")
-
-    // !false == true
-    tautology.expressions.clear()
-    tautology.expressions.add(simpleBooleanWithNegation)
-    logger.v("", "Evaluating simple boolean with negation [ ${tautology.evaluate()} ]")
-
-    // false && true == false
-    tautology.expressions.clear()
-    tautology.expressions.add(simpleBooleanWithAndOperation)
-    tautology.expressions.add(simpleBoolean2)
-    logger.v("", "Evaluating simple boolean with '&&' operation [ ${tautology.evaluate()} ]")
-
-    // false || true == true
-    tautology.expressions.clear()
-    tautology.expressions.add(simpleBooleanWithOrOperation)
-    tautology.expressions.add(simpleBoolean2)
-    logger.v("", "Evaluating simple boolean with '||' operation [ ${tautology.evaluate()} ]")
-
-    // false && false == false
-    tautology.expressions.clear()
-    tautology.expressions.add(simpleBooleanWithAndOperation)
-    tautology.expressions.add(simpleBoolean)
-    logger.v("", "Evaluating 'false && false' [ ${tautology.evaluate()} ]")
-
-    // false || false == false
-    tautology.expressions.clear()
-    tautology.expressions.add(simpleBooleanWithOrOperation)
-    tautology.expressions.add(simpleBoolean)
-    logger.v("", "Evaluating 'false || false' [ ${tautology.evaluate()} ]")
 
 }
 
