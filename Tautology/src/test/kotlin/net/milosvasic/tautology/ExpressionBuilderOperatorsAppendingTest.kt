8aff3f11ef5884632ebb39e84b53e15904d4544b
Milos Vasic <milos85vasic@gmail.com>
2017-04-20 14:29:57 +0200

    Tautology: Appending multiple expressions - test.

--- Tautology/src/test/kotlin/net/milosvasic/tautology/ExpressionBuilderOperatorsAppendingTest.kt
+++ Tautology/src/test/kotlin/net/milosvasic/tautology/ExpressionBuilderOperatorsAppendingTest.kt
@@ -1,10 +1,6 @@
 package net.milosvasic.tautology
 
 import net.milosvasic.logger.SimpleLogger
-import net.milosvasic.tautology.expression.BooleanExpression
-import net.milosvasic.tautology.expression.Expression
-import net.milosvasic.tautology.expression.ExpressionValue
-import net.milosvasic.tautology.expression.MultipleExpression
 import net.milosvasic.tautology.expression.builder.ExpressionBuilder
 import net.milosvasic.tautology.operator.Operator
 import org.junit.Assert
@@ -17,15 +13,18 @@ class ExpressionBuilderOperatorsAppendingTest {
     val logger = SimpleLogger()
     val dataSets = mutableListOf<DataSet>()
 
-    @Test
-    fun testOperatorsAppending() {
-        dataSets.clear()
+    @Before
+    fun prepareDataSets() {
         dataSets.add(DataSet(0, 0, 0, true))
         dataSets.add(DataSet(1, 1, 1, true))
         dataSets.add(DataSet(1, 1, 2, true))
         dataSets.add(DataSet(1, 2, 1, true))
         dataSets.add(DataSet(2, 1, 1, true))
         dataSets.add(DataSet(1, 2, 3, false))
+    }
+
+    @Test
+    fun testOperatorsAppending() {
         dataSets.forEach {
             (a, b, c, result) ->
             val builder = ExpressionBuilder()
@@ -48,22 +47,30 @@ class ExpressionBuilderOperatorsAppendingTest {
 
     @Test
     fun testOperatorsAppendingWithMultipleExpression() {
-//        dataSets.clear()
-//        // ((1 == 1) || (1 == 1)) && (1 >= 1)
-//        dataSets.add(DataSet(1, 1, 1, true))
-//        // ((1 == 2) || (1 == 3)) && (3 >= 1)
-//        dataSets.add(DataSet(1, 2, 3, false))
-//        // ((3 == 2) || (3 == 3)) && (3 >= 1)
-//        dataSets.add(DataSet(3, 2, 3, true))
-//        // ((3 == 4) || (3 == 3)) && (3 >= 1)
-//        dataSets.add(DataSet(3, 4, 3, true))
-//        // ((a == b) || (a == c)) && (c >= a)
-//        dataSets.forEach {
-//            (a, b, c, result) ->
-//            println("(($a == $b) || ($a == $c)) && ($c >= $a) expects: $result")
-//
-//
-//        }
+        dataSets.forEach {
+            (a, b, c, result) ->
+            val builder = ExpressionBuilder()
+                    .append(a == b)
+                    .append(Operator.OR())
+                    .append(a == c)
+                    .append(Operator.OR())
+                    .append(b == c)
+                    .append(Operator.AND())
+                    .append(
+                            ExpressionBuilder()
+                                    .append(a > c)
+                                    .append(Operator.OR())
+                                    .append(c > b)
+                    )
+
+            val expressions = builder.build()
+//            if (result) {
+//                Assert.assertTrue(tautology.evaluate(expressions))
+//            } else {
+//                Assert.assertFalse(tautology.evaluate(expressions))
+//            }
+            tautology.evaluate(expressions)
+        }
     }
 
 }
