2acef2d4e5dbde8cff691842c94d559d9a6a1380
Milos Vasic <milos85vasic@gmail.com>
2017-04-24 07:41:33 +0200

    Tautology: Working on 'misc' test.

---
+++ Tautology/src/test/kotlin/net/milosvasic/tautology/TautologyMiscTest.kt
@@ -0,0 +1,20 @@
+package net.milosvasic.tautology
+
+import org.junit.Test
+
+class TautologyMiscTest {
+
+    val a = 1
+    val b = 2
+    val c = 3
+    val d = 4
+    val e = 5
+
+    @Test
+    fun testMisc(){
+
+    }
+
+
+}
+
